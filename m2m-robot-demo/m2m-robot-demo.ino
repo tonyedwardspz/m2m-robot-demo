ee97c3cdbcb8ce151ecbdde0e357b9849df249dd
Tony Edwards <tony.edwards@gmail.com>
2018-03-25 20:00:01 +0100

    Updates robot code to use starter files as base
    Original demo code used a now defunct class structure. Ported the working code into the starter file structure because…. well… thats why they were written.

--- m2m-robot-demo/m2m-robot-demo.ino
+++ m2m-robot-demo/m2m-robot-demo.ino
@@ -9,6 +9,8 @@ namespace
 
   auto speed = 200;
 
+  volatile char command[2] = " ";
+
   // Instantiate the two motor objects, passing in the side of the robot the motor is on
   Motor left_motor{"left"};
   Motor right_motor{"right"};
@@ -20,6 +22,7 @@ namespace
 void setup()
 {
   Serial.begin(115200);
+  serial_flush();
   pinMode(interrupt_pin_2, INPUT_PULLUP);
   pinMode(interrupt_pin_3, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(interrupt_pin_2), left_motor_pulse_interrupt, RISING);
@@ -28,56 +31,84 @@ void setup()
 
 void loop()
 {  
-  delay(1000);
-
-  // Motor Usage
-  left_motor.forward(speed);
-  right_motor.forward(speed);
-
-  delay(1000);
-
-  left_motor.reverse(speed);
-  right_motor.reverse(speed);
-
-  delay(1000);
-
-  left_motor.stop();
-  right_motor.stop();
-
-
-  // Distance sensor usage
-  int dist_forwards = sensor.distance_forwards();
-  int dist_down = sensor.distance_down();
-
-  Serial.print("\n");
-  Serial.print("\nDistance Forwards: ");
-  Serial.print(dist_forwards);
-  Serial.print("\nDistance Down: ");
-  Serial.print(dist_down);
-
-
-  // Light Sensor Usage
-  int left_light = sensor.left_light_sensor();
-  int center_light = sensor.center_light_sensor();
-  int right_light = sensor.right_light_sensor();
-
-  Serial.print("\nLeft: ");
-  Serial.print(left_light);
-  Serial.print("\nCenter: ");
-  Serial.print(center_light);
-  Serial.print("\nRight: ");
-  Serial.print(right_light);
-
-
-  // Interupt Usage
-  int left_pulses = left_motor.get_pulse_count();
-  int right_pulses = right_motor.get_pulse_count();
-
-  Serial.print("\nLeft Pulse Count: ");
-  Serial.print(left_pulses);
-  Serial.print("\nRight Pulse Count: ");
-  Serial.print(right_pulses);
+  check_for_keypress();
 }
+
+void check_for_keypress() 
+{
+  if(Serial.available()>0) {
+    command[2] = Serial.read();
+    Serial.flush();
+  }
+  switch(command[2]) {
+  case 'u':// up
+    left_motor.forward(speed);
+    right_motor.forward(speed);
+    break;
+  case 'd':// down
+    left_motor.reverse(speed);
+    right_motor.reverse(speed);
+    break;
+  case 'q': // left
+    left_motor.forward(speed);
+    right_motor.reverse(speed);
+    break;
+  case 'e': // right
+    left_motor.reverse(speed);
+    right_motor.forward(speed);
+    break;
+  case 's': // stop
+    left_motor.stop();
+    right_motor.stop();
+    break;
+  case 'a':
+    action_a();
+    break;
+  case 'b':
+    action_b();
+    break;
+  case 'x':
+    action_x();
+    break;
+  case 'y':
+    action_y();
+    break;
+  }
+}
+
+// Follow the light
+void action_a()
+{
+  
+}
+
+// User programmed
+void action_b()
+{
+  
+}
+
+// Run away
+void action_x()
+{
+  
+}
+
+// Follow the light
+void action_y()
+{
+  
+}
+
+
+
+
+void serial_flush(){
+  while(Serial.available() > 0) {
+    char t = Serial.read();
+  }
+} 
+
 
 void left_motor_pulse_interrupt()
 {
